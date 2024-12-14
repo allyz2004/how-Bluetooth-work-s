5000938e4b171b2b7ced6293bf3ed753e021fe93
allyz2004 <youssef.alalya2015@gmail.com>
2024-12-14 12:03:40 +0300

    Mane.py
    PLEASE open in python.

---
+++ Mane.py
@@ -0,0 +1,10 @@
+#please open this in python IDLE.
+print ("so baseicaly, bluetooth communicates with each other using ultra-high frequency (UHF) radio waves.")
+print ("but thairs MORE!")
+print ("Bluetooth technology connects PCs and mobile devices to peripherals such as wireless headphones, keyboards, mice, and gaming controllers. Bluetooth devices communicate via short-range radio transmissions on the 2.4 GHz frequency range.
+
+The 2.4 GHz frequency range offers numerous channels that Bluetooth devices can take advantage of for communication. Paired devices jump between these channels in lockstep, constantly seeking the least interference and best signal quality. This process, known as frequency hopping, helps Bluetooth devices deliver low-latency, consistent performance.
+
+Using Bluetooth devices for the first time with your mobile device or PC requires an initial configuration process known as pairing, where each device exchanges essential information such as device IDs and security keys. Users often need to input a PIN or keycode on one or both devices to authenticate the pairing process. After the initial process, vital connection information is saved to streamline future pairings.
+
+Currently, Bluetooth technology is offered in two distinct versions, Bluetooth Classic and Bluetooth Low Energy (LE). Bluetooth Low Energy is optimized for burst communications that preserve battery life, while Bluetooth Classic is used for applications that require more-frequent and persistent data transfer. Bluetooth Classic has up to 79 2.4 GHz channels at its disposal, while Bluetooth Low Energy has up to 40 channels to hop between.")
